d438d8f1a5ae00447818e6ee747189894ba955f6
mel-am <122634403+mel-am@users.noreply.github.com>
2024-03-21 18:29:16 +0000

    The Document Object Model workshop

---
+++ week 01/JS/Variables/app.js
@@ -0,0 +1,18 @@
+console.log("Hello world!");
+
+let name = "Bobby";
+console.log(name); // Output: Bobby
+
+let age = 40;
+console.log(age); // Output: 40
+
+let car = "vw";
+console.log(car);
+
+let a = 4;
+let b = 8;
+console.log(a + b);
+
+let c = 90;
+let d = 180;
+console.log(c / d);
